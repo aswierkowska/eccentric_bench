b18e1d409ee3c84dd42635c7c828ca1b090fce2d
AleksandraSwierkowska <aleksandraswierkowska99@gmail.com>
2025-01-26 20:33:04 +0100

    Add Surface Code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append("/home/aswierkowska/eccentric_bench/external/qiskit_qec/src")
+import os
+sys.path.append(os.path.join(os.getcwd(), "external/qiskit_qec/src"))
 
 import stim
 import pymatching
@@ -17,11 +18,13 @@ from qiskit_qec.circuits import SurfaceCodeCircuit, CSSCodeCircuit
 from qiskit_qec.codes.hhc import HHC
 from qiskit_qec.utils import get_stim_circuits, noisify_circuit
 from qiskit_qec.noise import PauliNoiseModel
+from qiskit.visualization import circuit_drawer
 
-#def get_code(code: str, d: int):
+# def get_code(code: str, d: int):
 #    surface_code = CSSCode.from_code_name("surface", 3)
 #    ft_prep = gate_optimal_prep_circuit(surface_code, zero_state=True, max_timeout=2)
 #    return ft_prep.circ
+
 
 def get_code(code_name: str, d: int):
     if code_name == "hh":
@@ -29,27 +32,43 @@ def get_code(code_name: str, d: int):
         css_code = CSSCodeCircuit(code, T=d)
         return css_code
     elif code_name == "surface":
-        code = SurfaceCodeCircuit(d=d, T=d)
-    
+        code = SurfaceCodeCircuit(d=d, T=1)
+        return code
 
 
 def map_circuit(circuit: QuantumCircuit, backend: str):
-    stim_gates = ['x', 'y', 'z', 'cx', 'cz', 'cy', 'h', 's', 's_dag', 'swap', 'reset', 'measure', 'barrier'] # only allows basis gates available in Stim
+    stim_gates = [
+        "x",
+        "y",
+        "z",
+        "cx",
+        "cz",
+        "cy",
+        "h",
+        "s",
+        "s_dag",
+        "swap",
+        "reset",
+        "measure",
+        "barrier",
+    ]  # only allows basis gates available in Stim
     if backend[:3] == "ibm":
         service = QiskitRuntimeService(instance="ibm-q/open/main")
         backend = service.backend(backend)
-        circuit = transpile(circuit, 
-            backend=backend,
-            basis_gates=stim_gates,
-            optimization_level=0
+        circuit = transpile(
+            circuit, backend=backend, basis_gates=stim_gates, optimization_level=0
         )
     return circuit
 
 
 def simulate_circuit(circuit: stim.Circuit, num_shots: int) -> int:
     sampler = circuit.compile_detector_sampler()
-    detection_events, observable_flips = sampler.sample(num_shots, separate_observables=True)
-    detector_error_model = circuit.detector_error_model(decompose_errors=True, approximate_disjoint_errors=True)
+    detection_events, observable_flips = sampler.sample(
+        num_shots, separate_observables=True
+    )
+    detector_error_model = circuit.detector_error_model(
+        decompose_errors=True, approximate_disjoint_errors=True
+    )
     matcher = pymatching.Matching.from_detector_error_model(detector_error_model)
     predictions = matcher.decode_batch(detection_events)
     num_errors = 0
@@ -60,14 +79,19 @@ def simulate_circuit(circuit: stim.Circuit, num_shots: int) -> int:
             num_errors += 1
     return num_errors / num_shots
 
+
 def generate_pauli_error(p: int) -> PauliNoiseModel:
     pnm = PauliNoiseModel()
-    pnm.add_operation("h", {"x": p / 3, "y": p / 3, "z": p / 3, "i": 1 - p}) # here the weights do NOT need to be normalized
-    #pnm.add_operation("cx", {"ix": 1, "xi": 1, "xx": 1})
-    #pnm.add_operation("id", {"x": 1})
-    #pnm.add_operation("reset", {"x": 1})
-    #pnm.add_operation("measure", {"x": 1})
-    #pnm.add_operation("x", {"x": 1, "y": 1, "z": 1})
+    pnm.add_operation(
+        "h", {"x": p / 3, "y": p / 3, "z": p / 3, "i": 1 - p}
+    )  # here the weights do NOT need to be normalized
+    # pnm.add_operation("cx", {"ix": 1, "xi": 1, "xx": 1})
+    # pnm.add_operation("id", {"x": 1})
+    # pnm.add_operation("measure", {"x": p / 3, "y": p / 3, "z": p / 3, "i": 1 - p})
+    # pnm.add_operation("reset", {"x": p / 3, "y": p / 3, "z": p / 3, "i": 1 - p})
+    # pnm.add_operation("reset", {"x": 1})
+    # pnm.add_operation("measure", {"x": 1})
+    # pnm.add_operation("x", {"x": 1, "y": 1, "z": 1})
     return pnm
 
 
@@ -75,27 +99,35 @@ def run_experiment(experiment_name, backend, code_name, d, num_samples, error_pr
     try:
         code = get_code(code_name, d)
         detectors, logicals = code.stim_detectors()
-        code.circuit['0'] = map_circuit(code.circuit['0'], backend)
-        
+        code.circuit["0"] = map_circuit(code.circuit["0"], backend)
+
         for state, qc in code.circuit.items():
             code.noisy_circuit[state] = noisify_circuit(qc, error_prob)
-        
-        stim_circuit = get_stim_circuits(
-            code.noisy_circuit['0'], detectors=detectors, logicals=logicals
-        )[0][0]
-        
-        logical_error_rate = simulate_circuit(stim_circuit, num_samples)
-        logging.info(f"{experiment_name} | Logical error rate for {code_name} with distance {d}, backend {backend}: {logical_error_rate}")
-    
-    except Exception as e:
-        logging.error(f"{experiment_name} | Failed to run experiment for {code_name}, distance {d}, backend {backend}: {e}")
 
-if __name__ == '__main__':
+        stim_circuit = get_stim_circuits(
+            code.noisy_circuit["0"], detectors=detectors, logicals=logicals
+        )[0][0]
+
+        # with open("stim_circuit.stim", "w") as file:
+        #    file.write(str(stim_circuit))
+
+        logical_error_rate = simulate_circuit(stim_circuit, num_samples)
+        logging.info(
+            f"{experiment_name} | Logical error rate for {code_name} with distance {d}, backend {backend}: {logical_error_rate}"
+        )
+
+    except Exception as e:
+        logging.error(
+            f"{experiment_name} | Failed to run experiment for {code_name}, distance {d}, backend {backend}: {e}"
+        )
+
+
+if __name__ == "__main__":
     logging.getLogger("qiskit").setLevel(logging.WARNING)
     logging.basicConfig(
-        filename='qecc_benchmark.log',
+        filename="qecc_benchmark.log",
         level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s'
+        format="%(asctime)s - %(levelname)s - %(message)s",
     )
 
     with open("experiments.yaml", "r") as f:
@@ -113,7 +145,15 @@ if __name__ == '__main__':
 
         with ProcessPoolExecutor() as executor:
             futures = [
-                executor.submit(run_experiment, experiment_name, backend, code_name, d, num_samples, error_prob)
+                executor.submit(
+                    run_experiment,
+                    experiment_name,
+                    backend,
+                    code_name,
+                    d,
+                    num_samples,
+                    error_prob,
+                )
                 for backend, code_name, d in parameter_combinations
             ]
 
