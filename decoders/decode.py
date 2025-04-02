b204fd50aaddfad311d19a4cdef95599f0a87e44
jannik.pflieger <jannik.pflieger@gmail.com>
2025-04-02 09:42:49 +0200

    working steane

--- decoders/decode.py
+++ decoders/decode.py
@@ -18,12 +18,12 @@ def decode(code_name: str, circuit: stim.Circuit, num_shots: int, decoder: str) 
     )
     if code_name == "steane":
         #stim circuit to file steane_circuit.stim
-        stim.Circuit.to_file(circuit, "steane_circuit.stim")
-        subprocess.run("stim diagram --in steane_circuit.stim --out steane_circuit.svg --type timeline-svg", shell=True)
+        #stim.Circuit.to_file(circuit, "gidney_circuit.stim")
+        #subprocess.run("stim diagram --in gidney.stim --out gidney_circuit.svg --type timeline-svg", shell=True)
         #subprocess.run("stim analyze_errors --allow_gauge_detectors --in steane_circuit.stim --out analyze.dem", shell=True)
         # read analyze.dem
         #dem = stim.DetectorErrorModel.from_file("analyze.dem")
-        print(circuit)
+        #print(circuit)
         dem = circuit.detector_error_model()
     else:
         dem = circuit.detector_error_model() # TODO do we need those: decompose_errors=True, approximate_disjoint_errors=True
