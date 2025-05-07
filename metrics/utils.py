66783f7ad5cad712f2d855b8cd6f3e008b0b6cad
jannik.pflieger <jannik.pflieger@gmail.com>
2025-05-07 15:24:56 +0200

    addded boilerplate code for the functions we want to have in metrics

--- metrics/utils.py
+++ metrics/utils.py
@@ -15,4 +15,38 @@ def count_total_gates_qiskit(circuit: qiskit.QuantumCircuit):
 
 def count_swaps_qiskit(circuit: qiskit.QuantumCircuit):
     gates = circuit.count_ops()
-    return gates["swap"]
+    if "swap" in gates:
+        return gates["swap"]
+    return 0
+
+def count_total_gates_stim(circuit: stim.Circuit):
+    """
+    Count the number of gates in a Stim circuit.
+    """
+    count = 0
+    for op in circuit.operations():
+        if op.name not in ["MEASURE", "RESET", "BARRIER"]:
+            count += 1
+    return count
+
+def stabilizer_fidelity():
+    return None
+
+def get_threshold_error_rate():
+    return None
+
+def get_resource_overhead():
+    return None
+
+def get_error_supression_factor():
+    return None
+
+
+if __name__ == "__main__":
+    # Example usage for stim
+    stim_circuit = stim.Circuit.generated(
+        "surface_code:rotated_memory_z",
+        rounds=3,
+        distance=5
+    )
+    print("Total gates in stim circuit:", count_total_gates_stim(stim_circuit))
