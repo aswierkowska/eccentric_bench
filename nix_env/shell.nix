a7111b2cda63992a87c6631b6293b6c66ed234a7
Aleksandra Świerkowska <aleksandraswierkowska99@gmail.com>
2025-05-12 15:10:08 +0000

    Experiment batch

---
+++ nix_env/shell.nix
@@ -0,0 +1,28 @@
+# shell.nix
+with import <nixpkgs> {};
+
+mkShell rec {
+  buildInputs = [
+    python3  # Add the Python version you need
+    python3Packages.virtualenv  # Ensure you have virtualenv available
+    cmake
+  ];
+
+  # Setting the LD_LIBRARY_PATH, if necessary, for additional libraries
+  NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
+    stdenv.cc.cc
+    zlib
+  ];
+  LD_LIBRARY_PATH = NIX_LD_LIBRARY_PATH;
+  NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
+
+  shellHook = ''
+    # Activate the virtual environment
+    if [ -d ".venv" ]; then
+      source .venv/bin/activate
+    else
+      echo "No virtual environment found, create it using python -m venv .venv"
+    fi
+  '';
+}
+
